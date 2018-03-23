ea50669717c31b67c6c55466cd7393816370eb29
Kalyan Krishna <kkrishna@microsoft.com>
2018-03-23 15:04:52 +0000

    Updated to 2017 and readme revised

--- UserSync/App_Start/WebApiConfig.cs
+++ UserSync/App_Start/WebApiConfig.cs
@@ -9,6 +9,9 @@ namespace UserSync
     {
         public static void Register(HttpConfiguration config)
         {
+            // Web API configuration and services
+
+            // Web API routes
             config.MapHttpAttributeRoutes();
 
             config.Routes.MapHttpRoute(
